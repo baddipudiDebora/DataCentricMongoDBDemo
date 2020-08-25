5d430135bc82a8f8dd0a8439710e390ccdbf1013
baddipudiDebora <mailtodeborah1@gmail.com>
2020-08-25 19:08:40 +0000

    added base.html  template for flask

--- run.py
+++ run.py
@@ -20,63 +20,10 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    data = []
-    with open("static/data/products.json", "r") as json_data:
-        data = json.load(json_data)
-        return render_template("index.html", page_title="Shop Online", products=data)
-
-
-@app.route('/flyers')
-def flyers():
-    return render_template("flyers.html", page_title="Flyers")
-
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        users = mongo.db.users
-        login_user = users.find_one({'name' : request.form['username']})
-        if login_user:
-                if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
-                    session['username'] = request.form['username']
-                return render_template('home.html', user = login_user, logged_in=True)
-        return 'Invalid username/password combination'
-    return render_template('login.html')
-
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    if request.method == 'POST':
-        users = mongo.db.users
-        existing_user = users.find_one({'name' : request.form['username']})
-
-        if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'name' : request.form['username'], 'password' : hashpass})
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
-        
-        return 'That username already exists!'
-
-    return render_template('register.html')
-
-@app.route('/home')
-def home():
-    return render_template("home.html", page_title="Start shopping")
-
-
-@app.route('/contact')
-def contact():
-    return render_template("contact.html", page_title="Contact Us")
-
-
-@app.route('/shoppingcart')
-def shoppingcart():
-    return render_template("shoppingcart.html", page_title="Shopping Cart")
-
+    return render_template("index.html", page_title="Shop Online")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-
 
